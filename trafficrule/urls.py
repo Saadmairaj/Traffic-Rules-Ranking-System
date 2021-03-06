6acfdffe48a953cae9558c17f830674c069464f9
Saad <46227224+Saadmairaj@users.noreply.github.com>
2021-03-06 15:08:20 +0530

    Remove unwanted import.

--- trafficrule/urls.py
+++ trafficrule/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 from application import views
