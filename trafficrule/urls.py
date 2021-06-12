9f1070e2dc342772a4d21bb8c51786d0a70ae050
Saad <46227224+Saadmairaj@users.noreply.github.com>
2021-06-12 12:01:32 +0530

    Chaange title and heading of admin.

--- trafficrule/urls.py
+++ trafficrule/urls.py
@@ -52,3 +52,6 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = 'RTO Administration'
+admin.site.site_title = 'RTO site admin'
